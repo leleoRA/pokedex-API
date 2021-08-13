c88391823409f931313154489f18b2138826682a
leleo <celso@respondeai.com.br>
2021-08-13 15:28:19 -0300

    Preparando revisão de código

--- src/schemas/LoginSchema.ts
+++
@@ -1,9 +0,0 @@
-import Joi from "joi";
-
-const LoginSchema  = Joi.object({
-    email: Joi.string().email().required(),
-    password: Joi.string().required(),
-    confirmPassword: Joi.string()
-});
-
-export default LoginSchema;
